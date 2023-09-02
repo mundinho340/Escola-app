3fb85789c8ca9f4220a927623ff00bae1863b8a5
Raimundo Chigarisso <72931424+mundinho340@users.noreply.github.com>
2023-09-02 20:26:02 -0300

    Create home.php

---
+++ ui/home.php
@@ -0,0 +1,220 @@
+<?php
+    session_start();
+    if (isset($_SESSION['username'])) {
+      include "../app/db.conn.php";
+      include '../app/helpers/user.php';
+      include '../app/helpers/conversations.php';
+      include '../app/helpers/timeAgo.php';
+      include '../app/helpers/last_chat.php';
+      $user = getUser($_SESSION['username'], $conn);
+      
+
+    }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>home</title>
+
+   <!-- font awesome cdn link  -->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.2/css/all.min.css">
+
+   <!-- custom css file link  -->
+   <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+
+<header class="header">
+   
+   <section class="flex">
+
+      <a href="home.html" class="logo">Tua Escola</a>
+
+      <form action="search.html" method="post" class="search-form">
+         <input type="text" name="search_box" required placeholder="search courses..." maxlength="100">
+         <button type="submit" class="fas fa-search"></button>
+      </form>
+
+      <div class="icons">
+         <div id="menu-btn" class="fas fa-bars"></div>
+         <div id="search-btn" class="fas fa-search"></div>
+         <div id="user-btn" class="fas fa-user"></div>
+         <div id="toggle-btn" class="fas fa-sun"></div>
+      </div>
+
+      <div class="profile">
+         <img src="../uploads/<?=$user['p_p']?>" class="image" alt="">
+         <h3 class="name"><?=$user['name']?></h3>
+         <p class="role">estudente</p>
+         <a href="profile.php
+         " class="btn">Ver Perfil</a>
+         <div class="flex-btn">
+            <a href="login.html" class="option-btn">login</a>
+            <a href="register.html" class="option-btn">register</a>
+         </div>
+      </div>
+
+   </section>
+
+</header>   
+
+<div class="side-bar">
+
+   <div id="close-btn">
+      <i class="fas fa-times"></i>
+   </div>
+
+   <div class="profile">
+      <img src="../uploads/<?=$user["p_p"]?>" class="image" alt="">
+      <h3 class="name"><?=$user['name']?></h3>
+      <p class="role">Estudante</p>
+      <a href="profile.php" class="btn">Ver Perfil</a>
+   </div>
+
+   <nav class="navbar">
+      <a href="home.php"><i class="fas fa-home"></i><span>home</span></a>
+      <a href="notas.php"><i class="fa-brands fa-readme"></i></i><span>Notas</span></a>
+      <a href="aulas.php"><i class="fas fa-graduation-cap"></i><span>Aulas</span></a>
+      <a href="teachers.php"><i class="fas fa-chalkboard-user"></i><span>Professores</span></a>
+      <a href="aulas.php"><i class="fa-duotone fa-users-rectangle"></i><span>Turmas</span></a>
+      <a href="rota.php"><i class="fa-solid fa-paper-plane"></i><span>Chat</span></a>
+      <a href="../logout.php"> <span>Sair</span></a>
+   </nav>
+
+</div>
+
+<section class="home-grid">
+
+   <h1 class="heading"></h1>
+
+   <div class="box-container">
+
+      
+   </div>
+
+</section>
+
+
+
+<section class="courses">
+
+   <h1 class="heading">Aulas Assistidas</h1>
+
+   <div class="box-container">
+
+      <div class="box">
+         <div class="tutor">
+            <img src="images/pic-2.jpg" alt="">
+            <div class="info">
+               <h3>john deo</h3>
+               <span>21-10-2022</span>
+            </div>
+         </div>
+         <div class="thumb">
+            <img src="images/thumb-1.png" alt="">
+            <span>10 videos</span>
+         </div>
+         <h3 class="title">complete HTML tutorial</h3>
+         <a href="playlist.html" class="inline-btn">view playlist</a>
+      </div>
+
+      <div class="box">
+         <div class="tutor">
+            <img src="images/pic-3.jpg" alt="">
+            <div class="info">
+               <h3>john deo</h3>
+               <span>21-10-2022</span>
+            </div>
+         </div>
+         <div class="thumb">
+            <img src="images/thumb-2.png" alt="">
+            <span>10 videos</span>
+         </div>
+         <h3 class="title">complete CSS tutorial</h3>
+         <a href="playlist.html" class="inline-btn">view playlist</a>
+      </div>
+
+      <div class="box">
+         <div class="tutor">
+            <img src="images/pic-4.jpg" alt="">
+            <div class="info">
+               <h3>john deo</h3>
+               <span>21-10-2022</span>
+            </div>
+         </div>
+         <div class="thumb">
+            <img src="images/thumb-3.png" alt="">
+            <span>10 videos</span>
+         </div>
+         <h3 class="title">complete JS tutorial</h3>
+         <a href="playlist.html" class="inline-btn">view playlist</a>
+      </div>
+
+      <div class="box">
+         <div class="tutor">
+            <img src="images/pic-5.jpg" alt="">
+            <div class="info">
+               <h3>john deo</h3>
+               <span>21-10-2022</span>
+            </div>
+         </div>
+         <div class="thumb">
+            <img src="images/thumb-4.png" alt="">
+            <span>10 videos</span>
+         </div>
+         <h3 class="title">complete Boostrap tutorial</h3>
+         <a href="playlist.html" class="inline-btn">view playlist</a>
+      </div>
+
+      <div class="box">
+         <div class="tutor">
+            <img src="images/pic-6.jpg" alt="">
+            <div class="info">
+               <h3>john deo</h3>
+               <span>21-10-2022</span>
+            </div>
+         </div>
+         <div class="thumb">
+            <img src="images/thumb-5.png" alt="">
+            <span>10 videos</span>
+         </div>
+         <h3 class="title">complete JQuery tutorial</h3>
+         <a href="playlist.html" class="inline-btn">view playlist</a>
+      </div>
+
+      <div class="box">
+         <div class="tutor">
+            <img src="images/pic-7.jpg" alt="">
+            <div class="info">
+               <h3>john deo</h3>
+               <span>21-10-2022</span>
+            </div>
+         </div>
+         <div class="thumb">
+            <img src="images/thumb-6.png" alt="">
+            <span>10 videos</span>
+         </div>
+         <h3 class="title">complete SASS tutorial</h3>
+         <a href="playlist.html" class="inline-btn">view playlist</a>
+      </div>
+
+   </div>
+
+   <div class="more-btn">
+      <a href="courses.html" class="inline-option-btn">view all courses</a>
+   </div>
+
+</section>
+<footer class="footer">
+</footer>
+<!-- custom js file link  -->
+<script src="js/script.js"></script>
+ 
+</body>
+</html>
